cebaba70cdbc9308fd7dd005cf9e601aaf0921b4
김혜정(molly.ouo) <molly.ouo@kakaocorp.com>
2024-07-29 11:39:06 +0900

    프로젝트 추가

---
+++ test-vue2/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ModalComp from "./ModalComp.vue";
+import { ref } from "vue";
+
+const showModal = ref(false);
+</script>
+
+<template>
+  <button id="show-modal" @click="showModal = true">Show Modal</button>
+
+  <Teleport to="body">
+    <!-- use the modal component, pass in the prop -->
+    <modal-comp :show="showModal" @close="showModal = false">
+      <template #header>
+        <h3>Custom Header</h3>
+      </template>
+    </modal-comp>
+  </Teleport>
+</template>
